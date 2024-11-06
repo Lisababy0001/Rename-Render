72a783b46568cee6cab446e4827554e60e91c8c1
Lisababy0001 <vijaysharmag0005@gmail.com>
2024-11-06 19:42:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,27 +7,27 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "")
+API_ID = os.environ.get("API_ID", "23499948")
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "b92cd19ae415002b5c2285aa50885a4b")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+BOT_TOKEN = os.environ.get("lisa_baby", "") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "VJ_Botz") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "lisa_baby") 
 
-             # Don't Remove Credit @VJ_Botz
+             # Don't Remove Credit @lisa_baby
              # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
              # Ask Doubt on telegram @KingVJ01
 
-DB_NAME = os.environ.get("DB_NAME", "renamevjbot")     
+DB_NAME = os.environ.get("DB_NAME", "lisa_baby")     
 
-DB_URL = os.environ.get("DB_URL", "")
+DB_URL = os.environ.get("vijaysharmag0005:<db_WY48C9Mr0lufvxns>@lisa.bwjtc.mongodb.net/?retryWrites=true&w=majority&appName=lisa", "")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
 START_PIC = os.environ.get("START_PIC", "https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5606411877').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '23499948').split()]
 
 PORT = os.environ.get("PORT", "8080")
 
